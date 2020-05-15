d3cc9c63a7a1496b9fede8a19c652b3b244c3d92
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-05-15 17:17:40 -0600

    End of security implemantation module

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
+        [stringLength(8,MinimunLength = 4, ErrorMessage = "You must specify password between 4 and 8n characters")]
         public string Password { get; set; }
     }
 }
