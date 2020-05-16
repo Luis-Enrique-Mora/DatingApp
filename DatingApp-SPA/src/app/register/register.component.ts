f89390331f65144790250dd0c258808d9c66c93e
Luis Enrique Mora <luisenriquemora71829310112@protonmail.com>
2020-05-16 00:38:10 -0600

    trying to deal with logIn, not working yet, parameter are not read

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -8,14 +9,19 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class RegisterComponent implements OnInit {
   @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
-  model: any = { };
-  constructor() { }
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   register(){
-    console.log(this.model);
+   this.authService.register(this.model).subscribe(() =>{
+     console.log('registration successful');
+   }, error =>{
+    console.log(error);
+   });
   }
 
   cancel(){
